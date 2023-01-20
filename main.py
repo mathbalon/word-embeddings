cb4e5b8fa5a1c58e2ceabd6a952f130b290d306e
mathbalon <matheus.balonecker@jusbrasil.com.br>
2023-01-19 21:06:00 -0400

    Change text var name

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import yaml
 
 with open("data/" + sys.argv[1], "r") as file:
-    sentences = file.readlines()
+    text = file.readlines()
 
 with open("settings.yml", "r") as file:
     settings = yaml.load(file, Loader=yaml.FullLoader)
