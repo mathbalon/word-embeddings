19c634833efdb9d33bd932e350dd90e3a7681fad
mathbalon <matheus.balonecker@jusbrasil.com.br>
2023-01-19 21:21:51 -0400

    Implement the create word data points

---
+++ create_word_data_points.py
@@ -0,0 +1,24 @@
+
+from typing import Tuple
+from preprocessing import text_preprocessing
+
+def create_word_data_points(text:list, window:int) -> Tuple[list, list]:
+    word_pairs = []
+    all_words = []
+    
+    for sentence in text:
+        sentence = text_preprocessing(sentence)
+        
+        words = sentence.split()
+        
+        all_words += words
+        
+        for i, word in enumerate(words):
+            for w in range(window):
+                if i + 1 + w < len(words):
+                    word_pairs.append([word] + [words[(i + 1 + w)]])
+
+                if i - w - 1 >= 0:
+                    word_pairs.append([word] + [words[(i - w - 1)]])
+
+    return word_pairs, all_words
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sys
 import yaml
 
+from create_word_data_points import create_word_data_points
+
 with open("data/" + sys.argv[1], "r") as file:
     text = file.readlines()
 
 with open("settings.yml", "r") as file:
     settings = yaml.load(file, Loader=yaml.FullLoader)
     
+word_pairs, all_words = create_word_data_points(text, settings["window"])
