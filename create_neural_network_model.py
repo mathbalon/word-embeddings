aed94fe63805b1c0acd01cd4b7174f0c135c59b6
mathbalon <matheus.balonecker@jusbrasil.com.br>
2023-01-19 21:43:21 -0400

    Implement neural network model

---
+++ create_neural_network_model.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+def create_neural_network_model(X, Y, embed_size):
+
+    input_layer = tf.keras.Input(shape=(X.shape[1],))
+    hidden_layer = tf.keras.layers.Dense(units=embed_size, activation='linear')(input_layer)
+    output_layer = tf.keras.layers.Dense(units=Y.shape[1], activation='softmax')(hidden_layer)
+
+    model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
+    model.compile(loss = 'categorical_crossentropy', optimizer = 'adam')
+
+    model.fit(
+        x=X, 
+        y=Y, 
+        batch_size=256,
+        epochs=10000
+        )
+
+    return model.get_weights()[0]
+
+def plot_results(weights, unique_word_dict):
+    embedding_dict = {}
+
+    for word in unique_word_dict.keys(): 
+        embedding_dict.update({
+            word: weights[unique_word_dict.get(word)]
+            })
+    
+    plt.figure(figsize=(10, 10))
+
+    for word in list(unique_word_dict.keys()):
+        coord = embedding_dict.get(word)
+        plt.scatter(coord[0], coord[1])
+        plt.annotate(word, (coord[0], coord[1]))
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 import yaml
+
+from create_neural_network_model import create_neural_network_model, plot_results
 from create_one_hot_matrices import create_one_hot_matrices
 from create_unique_word_dict import create_unique_word_dict
 
@@ -16,3 +18,7 @@ word_pairs, all_words = create_word_data_points(text, settings["window"])
 unique_word_dict = create_unique_word_dict(all_words)
 
 X, Y = create_one_hot_matrices(word_pairs, unique_word_dict)
+
+weights = create_neural_network_model(X, Y, settings["embed_size"])
+
+plot_results(weights, unique_word_dict)
